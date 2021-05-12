937cf5430a9525058ef8afedf6fadfaeca244c40
Turan Assylkhan <turan@strongte.am>
2021-05-12 13:22:16 +0600

    feat: change name to login

--- Messenger/LoginViewController.swift
+++ Messenger/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Messenger
 //
 //  Created by Turan Assylkhan on 12.05.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
